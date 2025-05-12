83bb2b24c33c6ef712bab37cff830fd21a51d043
tommelt <thomas.meltzer1@gmail.com>
2025-05-12 08:02:37 +0100

    feat: refactor and add plot-height

--- src/bplot/bplot.py
+++ src/bplot/bplot.py
@@ -64,7 +64,20 @@ help_text = (
     help="range of indices to plot e.g., for a 2D array to plot the first 8 elements from the first row use --plot-range=0,1:8.",
     required=True,
 )
-def main(filename, dtype, shape, plot_range):
+@click.option(
+    "--layout",
+    "-l",
+    type=click.Choice(["f", "c"]),
+    help="layout in memory, f = fortran or c = c/c++.",
+    required=True,
+)
+@click.option(
+    "--plot-height",
+    type=int,
+    default=0,
+    help="set plot height. (default = 0, which will use full height of terminal).",
+)
+def main(filename, dtype, shape, plot_range, layout, plot_height):
     """
     \b
      █████               ████            █████
@@ -81,13 +94,15 @@ def main(filename, dtype, shape, plot_range):
     \b
     bplot can be used to plot binary data ouput from debuggers like gdb, mdb etc.
     """
-    shape = np.array(list(map(int, re.findall(r"(\d+)", shape)))).transpose()
+    shape = np.array(list(map(int, re.findall(r"(\d+)", shape))))
 
     if len(plot_range) > 15:
         print("Error :: --plot-range should be less than 15 chars long")
         exit(1)
     elif not all(c in "1234567890-[],: " for c in plot_range):
-        print('Error :: --plot-range can only consist of the following chars: "1234567890-,: "')
+        print(
+            'Error :: --plot-range can only consist of the following chars: "1234567890-,: "'
+        )
         exit(1)
     else:
         try:
@@ -96,21 +111,30 @@ def main(filename, dtype, shape, plot_range):
             print(f"Error :: cannot parse --plot-range [{plot_range!r}].")
             exit(1)
 
+    bin_data = read_binary_data(
+        filename=filename.name, shape=shape, dtype=dtype, layout=layout
+    )
+
+    plot_data(data=bin_data[slice_], plot_height=plot_height)
+
+
+def read_binary_data(filename, shape, dtype, layout):
+
     # Open the file in binary read mode
-    with open(filename.name, "rb") as file:
+    with open(filename, "rb") as file:
         bin_data = file.read()
 
     try:
         dtype_size = DTYPE_SIZES[dtype]
     except KeyError:
-        print(f"Error :: dtype [{dtype}] not recognized. Supported types are {DTYPE_SIZES.keys()}.")
+        print(
+            f"Error :: dtype [{dtype}] not recognized. Supported types are {DTYPE_SIZES.keys()}."
+        )
 
     # Ensure the data length is a multiple of dtype_size
     if len(bin_data) % dtype_size != 0:
         print(
-            f"Warning :: file size ({len(bin_data)} bytes) is not a multiple of {
-                dtype_size
-            }. Truncating extra bytes."
+            f"Warning :: file size ({len(bin_data)} bytes) is not a multiple of {dtype_size}. Truncating extra bytes."
         )
         bin_data = bin_data[: len(bin_data) - (len(bin_data) % dtype_size)]
 
@@ -120,8 +144,21 @@ def main(filename, dtype, shape, plot_range):
     # Unpack all doubles
     unpacked_data = struct.unpack(f"{num_values}{dtype}", bin_data)
 
-    bin_data = np.array(unpacked_data).reshape(shape.transpose()).transpose()
-    plt.scatter(bin_data[slice_])
+    if layout == "f":
+        bin_data = np.array(unpacked_data).reshape(shape[::-1]).transpose()
+    else:
+        bin_data = np.array(unpacked_data).reshape(shape)
+
+    return bin_data
+
+
+def plot_data(data, plot_height):
+
+    (w, h) = plt.ts()  # find terminal width and height
+    if plot_height > 0:
+        h = plot_height
+    plt.plot_size(w, h)
+    plt.scatter(data)
     plt.title(" ".join(sys.argv))
     plt.show()
 
